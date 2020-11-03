8440a7fb4ded45a8f53e55a879cd376c071fe24d
Hector_Jimenez <hjmontemayor@gmail.com>
2020-11-03 16:19:49 -0500

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,23 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord[5.1]
+    def change 
+        create_table costume_stores do |t|
+            t.string :name
+            t.string :location
+            t.integer :costume_inventory
+            t.integer :number_of_eployees
+            t.boolean :still_in_business?
+            t.string :opening_time
+            t.string :closing_time
+        end
+    end
+end
+
+
+# name
+# location
+# number of costumes, or "costume inventory"
+# number of employees
+# whether or not it's still in business
+# opening time
+# closing time
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,25 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord[5.1]
+    def change 
+        create_table haunted_houses do |t|
+            t.string :name
+            t.string :location
+            t.string :theme
+            t.integer :price
+            t.boolean :family_friendly?
+            t.string :opening_time
+            t.string :closing_time
+            t.string :long_description
+        end
+    end
+end
+
+
+# name
+# location
+# theme
+# price
+# whether they're family friendly or not
+# opening date
+# closing date
+# long description
